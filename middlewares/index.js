0041db01f3bbb04f4e1548057fb60bb55b499776
Sanjeet Boora <sanjeet.boora97@gmail.com>
2022-04-20 19:46:01 +0530

    code added

---
+++ middlewares/index.js
@@ -0,0 +1,4 @@
+const requestValidator = require('./requestValidator');
+const authJwt = require('./authJwt');
+const verifySignUp = require('./verifysignUp');
+module.exports = {requestValidator, authJwt, verifySignUp};
